14631797f343c58d3fbb5330cb426543de100d65
Adam Wespiser <adamwespiser@gmail.com>
2014-05-22 16:08:59 -0400

    adding in code to download encode meta data

--- analysis/downloadData/getENCODE_ChIP_Seq.R
+++ analysis/downloadData/getENCODE_ChIP_Seq.R
@@ -4,61 +4,88 @@ projectDir <- normalizePath(file.path(script.dir(), ".."))
 getFullPath <- function(subpath){ file.path(projectDir, subpath) }
 
 
+jan11FreezeUrl <<- "http://ftp.ebi.ac.uk/pub/databases/ensembl/encode/integration_data_jan2011/files.txt"
+jan11FreezeLocal <<- getFullPath("data/integration_data_jan2011-files.txt")
+jan11FreezeLocalTab <<- getFullPath("data/integration_data_jan2011-files.tab")
+
+
+cshlRNAseqUrl <<- "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeCshlLongRnaSeq/files.txt"
+cshlRNAseqLocal <<- getFullPath("data/wgEncodeCshlLongRnaSeq-files.txt")
+cshlRNAseqLocalTab <<- getFullPath("data/wgEncodeCshlLongRnaSeq-files.tab")
+
+sydhTfbsUrl <<- "http://hgdownload.cse.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeSydhTfbs/files.txt"
+sydhTfbsLocal <<- getFullPath("data/wgEncodeSydhTfbs-files.txt")
+sydhTfbsLocalTab <<- getFullPath("data/wgEncodeSydhTfbs-files.tab")
+
 
 
 convertToDfWithCols <- function(str,recSep,labelSep,valRm="",cols){
-	  records <- as.character(unlist(strsplit(str, recSep)))
+  records <- unique(as.character(unlist(strsplit(str, recSep))))
   nms <- as.character(unlist(sapply(records, function(x)unlist(strsplit(x, labelSep))[1])))
-    vals <- as.character(unlist(sapply(records, function(x)unlist(strsplit(x, labelSep))[2])))
-    # vals <- sapply(vals, function(x)gsub(x,";",""))
-    l <- as.list(vals)
-	  names(l) <- nms 
-	  df <- as.data.frame(l)
-	    cols.notfound <- cols[-which(cols %in% nms)]
-	    for ( i in cols.notfound){
-			    df[[i ]] <- NA
-		  }
-		  df
+  vals <- as.character(unlist(sapply(records, function(x)unlist(strsplit(x, labelSep))[2])))
+  # vals <- sapply(vals, function(x)gsub(x,";",""))
+  l <- as.list(vals)
+  names(l) <- nms 
+  df <- as.data.frame(l)
+  cols.notfound <- cols[-which(cols %in% nms)]
+  for ( i in cols.notfound){
+    df[[ i ]] <- NA
+  }
+  #remove extra cols 
+  df.cols <- colnames(df)
+  for (j in df.cols[-which(df.cols %in% cols)]){
+    df[[ j ]] <- NA
+  }
+  
+  df
 }
 
 
 
 convertToList <- function(str,recSep,labelSep,valRm=""){
-	  records <- as.character(unlist(strsplit(str, recSep)))
+  records <- as.character(unlist(strsplit(str, recSep)))
   nms <- as.character(unlist(sapply(records, function(x)unlist(strsplit(x, labelSep))[1])))
-    vals <- as.character(unlist(sapply(records, function(x)unlist(strsplit(x, labelSep))[2])))
-    # vals <- sapply(vals, function(x)gsub(x,";",""))
-    l <- as.list(vals)
-	  names(l) <- nms 
-	  l}
+  vals <- as.character(unlist(sapply(records, function(x)unlist(strsplit(x, labelSep))[2])))
+  # vals <- sapply(vals, function(x)gsub(x,";",""))
+  l <- as.list(vals)
+  names(l) <- nms 
+  l}
 
 shortenIdVec <- function(gene_id, sep = "\\."){
-	  as.character(sapply(gene_id, function(x)unlist(strsplit(x,split=sep))[1]))
+  as.character(sapply(gene_id, function(x)unlist(strsplit(x,split=sep))[1]))
 }
 
 
 
 
-
-
-
-
-fetchEncodeDccFilesText <- function(filesTxt="~/data/wgEncodeCshlLongRnaSeqFiles.txt",
-									                                    filesTxtTab="~/data/wgEncodeCshlLongRnaSeqFiles.tab"){
-	  if (!file.exists(filesTxt)){
-		      download.file("http://hgdownload.cse.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeCshlLongRnaSeq/files.txt",dest=filesTxt)
-	    }
+fetchEncodeFilesTxt <- function(urlAddr,localAddr,localTabAddr){
+  if (!file.exists(localAddr)){
+    download.file(urlAddr,dest=localAddr)
+  }
   
-  d <- readLines(filesTxt)
-    d.split <- lapply(d, function(x)strsplit(x,split="\t"))
-    d.names <- sapply(d.split, function(x)unlist(x)[1])
-	  d.info <- sapply(d.split, function(x)unlist(x)[2])
-	  d.list <- lapply(d.info,function(x)convertToList(x,"; ", "=",";"))
-	    colAnnot <-  unique(do.call("c",lapply(d.list, function(x)names(x))))
-	    df <- do.call(rbind,lapply(d.info,function(x)convertToDfWithCols(x,"; ", "=",";",cols=colAnnot)))
-		  df$filename <- d.names
-		  
-		  exportAsTable(df=df,file=filesTxtTab)
+  d <- readLines(localAddr)
+  if(length(grep(d,pattern="#"))){
+    d <- d[-grep(d,pattern="#")]
+  }
+  
+  d.split <- lapply(d, function(x)strsplit(x,split="\t"))
+  d.names <- sapply(d.split, function(x)unlist(x)[1])
+  d.info <- sapply(d.split, function(x)unlist(x)[2])
+  d.list <- lapply(d.info,function(x)convertToList(x,"; ", "=",";"))
+  colAnnot <-  unique(do.call("c",lapply(d.list, function(x)names(x))))
+  df <- do.call(rbind,lapply(d.info,function(x)convertToDfWithCols(x,"; ", "=",";",cols=colAnnot)))
+  df$filename <- d.names
+  
+  exportAsTable(df=df,file=localTabAddr)
 }
 
+
+getCtcfWithRNASeq <- function(){
+  rnaSeq <- read.csv(file=cshlRNAseqLocalTab,sep="\t",stringsAsFactor=FALSE)
+  
+  
+}
+
+
+sydhTfbsLocalTab
 
--- analysis/main.R
+++ analysis/main.R
@@ -1,7 +1,11 @@
 script.dir <- function() {
-	  # from http://stackoverflow.com/a/16046056
-	  dirname(sys.frame(1)$ofile)
+  # from http://stackoverflow.com/a/16046056
+  dirname(sys.frame(1)$ofile)
 }
+
+## Set to true if you want to download dependencies/packages
+downloadPackages <<- FALSE
+
 
 ### Start with project dir, and helper functions
 projectDir <- normalizePath(file.path(script.dir(), ".."))
@@ -19,55 +23,88 @@ clear <- function(save.vec=c()){ ls.vec <- ls(globalenv());del.vec <-setdiff(ls.
 readInTable <- function(file) read.table(file=file,stringsAsFactors=FALSE,header=TRUE)
 
 
+source(getFullPath("analysis/downloadData/getENCODE_ChIP_Seq.R"))
+
+
+
+
+
+
+
+
 # setup libs
 # install if needed from http://stackoverflow.com/a/4090208
 list.of.packages <- c(
-					    "ggplot2",
-						  "ROCR", # http://cran.r-project.org/web/packages/ROCR/index.html
-						    "glmnet", # http://cran.r-project.org/web/packages/glmnet/glmnet.pdf
-							  "randomForest", #http://cran.at.r-project.org/web/packages/randomForest/randomForest.pdf
-							    "doParallel",
-								  "foreach",
-								    "mboost",
-									  "vcd", # mosaicpl
-									    "C50", # kuhn:411
-										  "mda", # fda, kuhn:362
-										    "gam",
-											  "reshape2", # needed for melt
-											    "MASS",
-												  "devtools", # for github installs
-												    "stringr",
-													  "plyr",
-													    "dplyr"
-						)
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+  "ggplot2",
+  "ROCR", # http://cran.r-project.org/web/packages/ROCR/index.html
+  "glmnet", # http://cran.r-project.org/web/packages/glmnet/glmnet.pdf
+  "randomForest", #http://cran.at.r-project.org/web/packages/randomForest/randomForest.pdf
+  "doParallel",
+  "foreach",
+  "mboost",
+  "vcd", # mosaicpl
+  "C50", # kuhn:411
+  "mda", # fda, kuhn:362
+  "gam",
+  "reshape2", # needed for melt
+  "MASS",
+  "devtools", # for github installs
+  "stringr",
+  "plyr",
+  "dplyr"
+)
 
-if (!("gbm" %in% installed.packages()) && "devtools" %in% install.packages()) {
-	  library(devtools)
-  install_url("http://cran.r-project.org/src/contrib/Archive/gbm/gbm_2.0-8.tar.gz")
+
+if(TRUE == downloadPackages ){
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages)
+  
+  if (!("gbm" %in% installed.packages()) && "devtools" %in% install.packages()) {
+    library(devtools)
+    install_url("http://cran.r-project.org/src/contrib/Archive/gbm/gbm_2.0-8.tar.gz")
+  }
+  
+  #install_github("harrysouthworth/gbm")
+  
+  #load libs
+  lapply(list.of.packages, function(lib){
+    library(lib, character.only=TRUE)
+  })
 }
 
-#install_github("harrysouthworth/gbm")
 
-#load libs
-lapply(list.of.packages, function(lib){
-	     library(lib, character.only=TRUE)
-						})
 
 calcNumCores <- function(){
-	  numCores <- detectCores()
+  numCores <- detectCores()
   if(numCores > 8){
-	      numCores <- numCores / 2
-    } else if(numCores == 1){
-		    numCores <- 1
-	  } else {
-		      numCores <- numCores - 1
-	    }
-    cat("using", numCores, "cores")
-    return(numCores)
+    numCores <- numCores / 2
+  } else if(numCores == 1){
+    numCores <- 1
+  } else {
+    numCores <- numCores - 1
+  }
+  cat("using", numCores, "cores")
+  return(numCores)
 }
 #registerDoParallel(calcNumCores())
 registerDoParallel(10)
+
+
+
+downloadJan2011FreezeFilesTxt <- function(){
+  fetchEncodeFilesTxt(urlAddr=jan11FreezeUrl,localAddr=jan11FreezeLocal,localTabAddr=jan11FreezeLocalTab)
+  fetchEncodeFilesTxt(urlAddr=cshlRNAseqUrl,localAddr=cshlRNAseqLocal,localTabAddr=cshlRNAseqLocalTab)
+  fetchEncodeFilesTxt(urlAddr=sydhTfbsUrl,localAddr=sydhTfbsLocal,localTabAddr=sydhTfbsUrlLocalTab)
+  
+}
+
+
+
+
+
+
+
+
+
 
 
