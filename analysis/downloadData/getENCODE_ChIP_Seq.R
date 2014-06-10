23e48aed4dcf38d2d05d0f5317aa53de64cda278
Adam Wespiser <adamwespiser@gmail.com>
2014-06-09 21:03:11 -0400

    updating ctcf to run macs2 on peaks

--- analysis/downloadData/getENCODE_ChIP_Seq.R
+++ analysis/downloadData/getENCODE_ChIP_Seq.R
@@ -3,7 +3,7 @@ getDataFile <- function(subpath){file.path(homeFolder, "data", subpath)}
 projectDir <- normalizePath(file.path(script.dir(), ".."))
 getFullPath <- function(subpath){ file.path(projectDir, subpath) }
 
-
+clusterDir <<- "/project/umw_zhiping_weng/wespisea/ctcf/data/"
 
 jan11FreezeUrl <<- "http://ftp.ebi.ac.uk/pub/databases/ensembl/encode/integration_data_jan2011/files.txt"
 jan11FreezeLocal <<- getFullPath("data/integration_data_jan2011-files.txt")
@@ -68,7 +68,6 @@ convertToDfWithCols <- function(str,recSep,labelSep,valRm="",cols){
 
 joinUnevenDfs <- function(df1,df2){
   stopifnot(is.data.frame(df1),is.data.frame(df2))
-  
   
   col1 <-   colnames(df1)
   col2 <- colnames(df2)
@@ -161,6 +160,33 @@ getCTCFchipAllFilesCellTypes <- function(){
   
 }
 
+
+getCTCFPeakSeq <- function(){
+  allExp <- read.csv(file=jan11FreezeLocalTab,sep="\t",stringsAsFactor=FALSE)
+  allExp.chSeq <- allExp[which(allExp$dataType %in% c("ChipSeq", "ChIP-seq")),]
+  allExp.all.celltypes <- unique(allExp.chSeq$antibody)
+  ctcf.antibody <- allExp.all.celltypes[grep(allExp.all.celltypes,pattern="CTCF",ignore.case=TRUE)]
+  if(grep(ctcf.antibody,pattern="CTCFL",ignore.case=TRUE)){
+    ctcf.antibody <- ctcf.antibody[-grep(ctcf.antibody,pattern="CTCFL",ignore.case=TRUE)]
+  }
+  
+  allExp.ctcf <- allExp[which(allExp$antibody %in% ctcf.antibody),]
+  allExp[which(allExp.ctcf$softwareVersion == "PeakSeq"),]
+}
+
+getCTCFSPP <- function(){
+  allExp <- read.csv(file=jan11FreezeLocalTab,sep="\t",stringsAsFactor=FALSE)
+  allExp.chSeq <- allExp[which(allExp$dataType %in% c("ChipSeq", "ChIP-seq")),]
+  allExp.all.celltypes <- unique(allExp.chSeq$antibody)
+  ctcf.antibody <- allExp.all.celltypes[grep(allExp.all.celltypes,pattern="CTCF",ignore.case=TRUE)]
+  if(grep(ctcf.antibody,pattern="CTCFL",ignore.case=TRUE)){
+    ctcf.antibody <- ctcf.antibody[-grep(ctcf.antibody,pattern="CTCFL",ignore.case=TRUE)]
+  }
+  
+  allExp.ctcf <- allExp[which(allExp$antibody %in% ctcf.antibody),]
+  allExp[which(allExp.ctcf$softwareVersion == "SPP"),]
+}
+
 getCTCFchipAllFiles <- function(){
   allExp <- read.csv(file=jan11FreezeLocalTab,sep="\t",stringsAsFactor=FALSE)
   allExp.chSeq <- allExp[which(allExp$dataType %in% c("ChipSeq", "ChIP-seq")),]
@@ -191,6 +217,16 @@ getCTCF_tfbs<- function(labList){
   chSeq[which(chSeq$antibody %in% ctcf.antibody),]
 }
 
+
+getInput_tfbs<- function(labList){
+  chSeq <- read.csv(file=labList$localTab,sep="\t",stringsAsFactor=FALSE)
+  chSeq$filename <- paste0(labList$base,chSeq$filename)
+  
+  chSeq[which(chSeq$setType == "input"),]
+}
+
+
+
 getTfbsUniqueCells <- function(df){
   stopifnot("cell" %in% colnames(df))
   if(dim(df)[1] == 0){
@@ -206,28 +242,128 @@ getAllTfbs <- function(){
   comb <- joinUnevenDfs(joinUnevenDfs(uw,haib),sydh) 
 }
 
-
+getAllTfbsInput <- function(){
+  uw <- getInput_tfbs(uw.list)
+  haib <- getInput_tfbs(haib.list)
+  sydh <- getInput_tfbs(sydh.list)
+  comb <- joinUnevenDfs(joinUnevenDfs(uw,haib),sydh) 
+  
+}
 
 
 
 
 getCtcfWithRNASeq <- function(){
    
-  tfbs <- getAllTfbs()
+  inputExp.all <- getAllTfbsInput()
+  inputExp.all$fileEnding <- sapply(inputExp.all$filename,getLastUrl)
+  
+  inputExp <- inputExp.all[which(inputExp.all$type %in% c("bai","bam")),]
+  
+  
+  tfbs.all <- getAllTfbs()
+  tfbs.all$fileEnding <- sapply(tfbs.all$filename,getLastUrl)
+  
+  tfbs.ab <- tfbs.all[which(tfbs.all$type %in% c("bai","bam")),]
+  tfbs <- merge(tfbs.ab,inputExp, by=c("project","grant","lab","cell","type","replicate","protocol"),suffix=c("",".cntrl"))
+  
   tfbs.cells <- getTfbsUniqueCells(tfbs)
    
   rnaSeq <- read.csv(file=cshlRNAseqLocalTab,sep="\t",stringsAsFactor=FALSE)
+  rnaSeq <- rnaSeq[which(rnaSeq$type == "fastq"),]
+  rnaSeq$fileEnding <- rnaSeq$filename
   rnaSeq$filename <- paste0(cshlRNAseqUrlBase,rnaSeq$filename)
   rnaSeq.lpa.wholeCell <- rnaSeq[which(rnaSeq$localization == "cell" & rnaSeq$rnaExtract == "longPolyA"),]
   rna.cells <- unique(rnaSeq.lpa.wholeCell$cell)
   
   cellTypes <- intersect(rna.cells,tfbs.cells)
-    
+  
   rnaSeq.peaks.df <-joinUnevenDfs(tfbs[which(tfbs$cell %in% cellTypes),],
                                   rnaSeq.lpa.wholeCell[which(rnaSeq.lpa.wholeCell$cell %in% cellTypes),])  
+  rnaSeq.peaks.df <- rnaSeq.peaks.df[which(rnaSeq.peaks.df$treatment %in% c("None",NA)),]
+  rnaSeq.peaks.df <- rnaSeq.peaks.df[which(rnaSeq.peaks.df$objStatus != "replaced - object is identifying wrong peaks" |
+                                             is.na(rnaSeq.peaks.df$objStatus)),]
+  rnaSeq.peaks.df <- rnaSeq.peaks.df[-which(rnaSeq.peaks.df$controlId.cntrl %in% c("wgEncodeEH000706","wgEncodeEH000771")),]
   
+  
+  
+  
+  
+  rnaSeq.peaks.df$localFile <- paste0(clusterDir,rnaSeq.peaks.df$fileEnding)
+  rnaSeq.peaks.df$downloadCmd <- paste0("wget --continue ",rnaSeq.peaks.df$filename,
+                                        " -O ",rnaSeq.peaks.df$localFile)
+  
+  rnaSeq.Cntr.idx <- !is.na(rnaSeq.peaks.df$fileEnding.cntrl)
+  rnaSeq.peaks.df$localFile.cntrl <- paste0(clusterDir,rnaSeq.peaks.df$fileEnding.cntrl)
+  
+  cntrlDownload <-  paste0("wget --continue ",rnaSeq.peaks.df$filename.cntrl,
+                           " -O ",rnaSeq.peaks.df$localFile.cntrl)[rnaSeq.Cntr.idx]
+  
+  totalDownload <- c(cntrlDownload,rnaSeq.peaks.df$downloadCmd)
+  
+  writeLines(con="~/sandbox/downloadCTCF_bamBaiControl",
+             text=paste(totalDownload,rep(c("&",""),length.out=length(totalDownload))))
+  # scpFile(file.local="~/sandbox/downloadCTCF_bamBaiControl", dir.remote="~/bin/") 
+  
+  
+  tfbs.peaks.full <- rnaSeq.peaks.df[rnaSeq.Cntr.idx,]
+
+  tfbs.peaks <- tfbs.peaks.full[-grep(x=tfbs.peaks.full$localFile, pattern="bai"),]
+  tfbs.peaks$cntrlName <- gsub(x=gsub(x=tfbs.peaks$fileEnding.cntrl,pattern="wgEncode",replacement=""),
+                                    pattern=".bam",replacement="")
+  tfbs.peaks$expName <- gsub(x=gsub(x=tfbs.peaks$fileEnding,pattern="wgEncode",replacement=""),
+                               pattern=".bam",replacement="")
+  tfbs.peaks$name <- with(tfbs.peaks,paste0(expName,"_",cntrlName))
+  macs2dir <- "/project/umw_zhiping_weng/wespisea/ctcf/macs2/"
+  tfbs.peaks$nameFile <- with(tfbs.peaks,paste0(macs2dir,name))
+  write(file="~/sandbox/macs2_ctcf_manifest", tfbs.peaks$nameFile)
+  # scpFile(file.local="~/sandbox/macs2_ctcf", dir.remote=macs2dir) 
+  
+  # /home/aw30w/bin/MACS-master/bin/macs2 callpeak -t ChIP.bam -c Control.bam -f BAM -g hs -n test -B -q 0.01
+  # https://github.com/taoliu/MACS/
+  macs2 <-  paste0("/home/aw30w/bin/MACS-master/bin/macs2 callpeak -t ",tfbs.peaks$localFile,
+                           " -c ",tfbs.peaks$localFile.cntrl,
+                           " -f BAM -g hs -B -q 0.01 ",
+                           " -n ",tfbs.peaks$nameFile )
+  write(file="~/sandbox/macs2_ctcf", macs2)
+  # scpFile(file.local="~/sandbox/macs2_ctcf", dir.remote="~/bin/") 
+  # perl /home/aw30w/bin/runTask.pl -f ~/bin/macs2_ctcf -c 24 -m 2024 -W 600 -Q short -t ctcfMacs2
+}
+
+getLastUrl <- function(x){
+  splitUrl <- as.character(unlist(strsplit(x,"/")))
+  splitUrl[length(splitUrl)]
 }
 
 
-sydhTfbsLocalTab
+getCtcfPeakSeqWithRNASeq <- function(){
+  
+  tfbs <- getCTCFSPP()
+  tfbs$fileEnding <- sapply(tfbs$filename,getLastUrl)
+  tfbs.cells <- getTfbsUniqueCells(tfbs)
+  
+  rnaSeq <- read.csv(file=cshlRNAseqLocalTab,sep="\t",stringsAsFactor=FALSE)
+  rnaSeq <- rnaSeq[which(rnaSeq$type == "fastq"),]
+  rnaSeq$fileEnding <- rnaSeq$filename
+  rnaSeq$filename <- paste0(cshlRNAseqUrlBase,rnaSeq$filename)
+  rnaSeq.lpa.wholeCell <- rnaSeq[which(rnaSeq$localization == "cell" & rnaSeq$rnaExtract == "longPolyA"),]
+  rna.cells <- unique(rnaSeq.lpa.wholeCell$cell)
+  
+  cellTypes <- intersect(rna.cells,tfbs.cells)
+  
+  rnaSeq.peaks.df <-joinUnevenDfs(tfbs[which(tfbs$cell %in% cellTypes),],
+                                  rnaSeq.lpa.wholeCell[which(rnaSeq.lpa.wholeCell$cell %in% cellTypes),])  
+  
+  rnaSeq.peaks.df$localFile <- paste0(clusterDir,rnaSeq.peaks.df$fileEnding)
+  rnaSeq.peaks.df$downloadCmd <- paste0("wget --continue ",rnaSeq.peaks.df$filename," -O ",rnaSeq.peaks.df$localFile)
+  
+  writeLines(con="~/sandbox/downloadCTCF",
+            text=paste(rnaSeq.peaks.df$downloadCmd,rep(c("&",""),times=length(rnaSeq.peaks.df$downloadCmd))))
+  
+  # scpFile(file.local="~/sandbox/downloadCTCF", dir.remote="~/bin/") 
+
+
+  
+  
+}
 
